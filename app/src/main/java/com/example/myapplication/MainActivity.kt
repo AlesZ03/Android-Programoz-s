93ad8d1f8d65c58a1077b0ee9a14628457baf3a7
Balázs Vajk-Istiván <balazsvajk2003@gmail.com>
2025-10-28 17:55:53 +0200

    Elso commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.labor_1
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.labor_1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.d(TAG, "onCreate: MainActivity created.")
+
+        // NavController beállítása
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // BottomNavigationView összekötése a NavController-rel
+        binding.bottomNav.setupWithNavController(navController)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: MainActivity started.")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: MainActivity resumed.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: MainActivity paused.")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: MainActivity stopped.")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: MainActivity restarted.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: MainActivity destroyed.")
+    }
+}
